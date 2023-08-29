9c5c332fd0ca8e1dd9a86ba6b0005c18335fc05f
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2023-08-29 17:19:32 +0530

    minor updates in mvn options
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- .jenkins/jenkins.groovy
+++ .jenkins/jenkins.groovy
@@ -58,8 +58,8 @@ def runSystemTests(String workspace, String testCases, String testProfile, Strin
     def testcasesTag = testCases.isEmpty() ? "" : "-Dit.test=${testCases} "
     withMaven(mavenOpts: '-Djansi.force=true') {
         sh(script: "mvn -f ${workspace}/systemtest/pom.xml verify " +
-            "-Pacceptance" +
-            "-Dgroups=bridge,metrics,mirrormaker,mirrormaker2" +
+            "-Pacceptance " +
+            "-Dgroups=bridge,metrics,mirrormaker,mirrormaker2 " +
             "-DexcludedGroups=${excludeGroups} " +
             "${testcasesTag}" +
             "-Djava.net.preferIPv4Stack=true " +
