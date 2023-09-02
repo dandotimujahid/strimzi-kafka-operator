f82e1cb12a0995d98cac6797e6d2dbbd6a0a5b88
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2023-09-02 20:06:14 +0530

    only running mirrormaker2
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- .jenkins/jenkins.groovy
+++ .jenkins/jenkins.groovy
@@ -59,7 +59,7 @@ def runSystemTests(String workspace, String testCases, String testProfile, Strin
     withMaven(mavenOpts: '-Djansi.force=true') {
         sh(script: "mvn -f ${workspace}/systemtest/pom.xml verify " +
             "-P${testProfile} " +
-            "-Dgroups=mirrormaker " +
+            "-Dgroups=mirrormaker2 " +
             "-DexcludedGroups=${excludeGroups} " +
             "${testcasesTag}" +
             "-Djava.net.preferIPv4Stack=true " +
