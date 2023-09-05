df7bfd805d6d940e6e0e5a079603e953cb3c3628
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2023-09-05 10:41:50 +0530

    only executing few test groups
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- .jenkins/jenkins.groovy
+++ .jenkins/jenkins.groovy
@@ -60,7 +60,8 @@ def runSystemTests(String workspace, String testCases, String testProfile, Strin
         sh(script: "mvn -f ${workspace}/systemtest/pom.xml verify " +
             "-P${testProfile} " +
             "${groupsTag}" +
-            "-DexcludedGroups=bridge,metrics,mirrormaker,mirrormaker2,oauth,loadbalancer,networkpolicies,flaky " +
+            "-Dgroups=bridge,metrics,mirrormaker,mirrormaker2,oauth " +
+            "-DexcludedGroups=loadbalancer,networkpolicies,flaky " +
             "${testcasesTag}" +
             "-Djava.net.preferIPv4Stack=true " +
             "-DtrimStackTrace=false " +
