15a95307e255f4817ca1548dae13461d8040f921
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2023-09-08 14:29:55 +0530

    only running metrics tests
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- .jenkins/jenkins.groovy
+++ .jenkins/jenkins.groovy
@@ -59,8 +59,8 @@ def runSystemTests(String workspace, String testCases, String testProfile, Strin
     withMaven(mavenOpts: '-Djansi.force=true') {
         sh(script: "mvn -f ${workspace}/systemtest/pom.xml verify " +
             "-P${testProfile} " +
-            "${groupsTag}" +
-            "-DexcludedGroups=bridge,metrics,mirrormaker,mirrormaker2,oauth,loadbalancer,networkpolicies,flaky " +
+            "-Dgroups=metrics " +
+            "-DexcludedGroups=bridge,acceptance,mirrormaker,mirrormaker2,oauth,loadbalancer,networkpolicies,flaky " +
             "${testcasesTag}" +
             "-Djava.net.preferIPv4Stack=true " +
             "-DtrimStackTrace=false " +
