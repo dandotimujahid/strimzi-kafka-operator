12ad35bb4c8845cdba304ceed64c61f00e027a3a
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2024-10-29 23:49:11 +0530

    Update KafkaNodePoolCrdIT.java
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- api/src/test/java/io/strimzi/api/kafka/model/nodepool/KafkaNodePoolCrdIT.java
+++ api/src/test/java/io/strimzi/api/kafka/model/nodepool/KafkaNodePoolCrdIT.java
@@ -41,7 +41,6 @@ public class KafkaNodePoolCrdIT extends AbstractCrdIT {
                 () -> createDeleteCustomResource("KafkaNodePool-with-invalid-role.yaml"));
 
         assertThat(exception.getMessage(), containsStringIgnoringCase("spec.roles[0]: Unsupported value: \"helper\": supported values: \"controller\", \"broker\""));
-        System.out.println(spec.roles[0]);
     }
 
     @BeforeAll
