e18760f43bdeb58fdccaf2c4dc2cfc3dadd9beb2
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2024-11-01 00:56:39 +0530

    rerun count 3
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- .jenkins/jenkins.groovy
+++ .jenkins/jenkins.groovy
@@ -66,7 +66,8 @@ def runSystemTests(String workspace, String testCases, String testProfile, Strin
             "-DtrimStackTrace=false " +
             "-Dstyle.color=always " +
             "--no-transfer-progress " +
-            "-Dfailsafe.rerunFailingTestsCount=2 " +
+            "-Dfailsafe.rerunFailingTestsCount=3 " +
+            "-Dskip.surefire.tests -s " +
             "-Djunit.jupiter.execution.parallel.enabled=${parallelEnabled} " +
             // sequence mode with testInParallel=1 otherwise parallel (default method-wide parallelism)
             "-Djunit.jupiter.execution.parallel.config.fixed.parallelism=${testsInParallel}")
