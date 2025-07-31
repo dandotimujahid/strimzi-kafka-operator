4d7e5298215e6bb787bb38d73cfee6b2a6771a3a
Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>
2025-07-31 14:59:55 +0530

    nginx 1.28.0
    Signed-off-by: Mujahid Dandoti <70288064+dandotimujahid@users.noreply.github.com>

--- .azure/scripts/setup-kubernetes.sh
+++ .azure/scripts/setup-kubernetes.sh
@@ -91,7 +91,7 @@ if [ "$TEST_CLUSTER" = "minikube" ]; then
 
     if [ "$ARCH" = "s390x" ]; then
         git clone -b v1.9.11 --depth 1 https://github.com/kubernetes/kubernetes.git
-        sed -i 's/:1.11/:1.26.3/' kubernetes/cluster/addons/registry/images/Dockerfile
+        sed -i 's/:1.11/:1.28.0/' kubernetes/cluster/addons/registry/images/Dockerfile
         docker build --pull -t gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH} kubernetes/cluster/addons/registry/images/
         minikube image load ${ARCH}/registry:2.8.2 gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH}
         minikube addons enable registry --images="Registry=${ARCH}/registry:2.8.2,KubeRegistryProxy=gcr.io/google_containers/kube-registry-proxy:0.4-${ARCH}"
